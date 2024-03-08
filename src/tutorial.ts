6d062881c9e88dea5a5d6610fbf6d2b2d0255578
AnnaLinden <linden.a.anna@gmail.com>
2024-03-08 19:47:12 +0200

    type annotations

---
+++ src/tutorial.ts
@@ -0,0 +1,18 @@
+
+let awesomeName:string = 'shakeAndBake'
+awesomeName = "something"
+awesomeName = awesomeName.toUpperCase();
+console.log(awesomeName);
+
+// awesomeName = 10;
+
+let amount: number = 10;
+amount += 10;
+console.log (amount);
+// amount = 'hello';
+
+let isEqual: boolean = true;
+isEqual = false;
+// isEqual = "cat";
+
+console.log(isEqual);
