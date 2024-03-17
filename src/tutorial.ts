1ca47319f6d29f27e1fb8431e227930faa79d958
AnnaLinden <linden.a.anna@gmail.com>
2024-03-17 20:41:24 +0200

    challenge functions

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -196,3 +196,18 @@ const newStudent = {
 };
 createStudent(newStudent);
 //createStudent({id:2, name:'aki', email: 'aki@email.com'});
+
+//challenge
+function processData(input: number|string, config:{reverse:boolean} = {reverse: false}){
+    if (typeof input === 'number' ) {
+        return input*input;
+    } else{
+        return config.reverse? input.toUpperCase().split('').reverse().join('')
+        :input.toUpperCase();
+    }
+
+}
+
+console.log(processData(10));
+console.log(processData('Hello'));
+console.log(processData('Hello', {reverse:true}));
