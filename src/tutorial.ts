cfbce20ff9886b7c35e189dc67d12007c435f26e
AnnaLinden <linden.a.anna@gmail.com>
2024-03-18 21:30:01 +0200

    type alias

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -211,3 +211,42 @@ function processData(input: number|string, config:{reverse:boolean} = {reverse: 
 console.log(processData(10));
 console.log(processData('Hello'));
 console.log(processData('Hello', {reverse:true}));
+
+//type alias
+
+type User = { id: number; name: string; isActive: boolean};
+
+const john: User = {
+    id: 1,
+    name: 'john',
+    isActive: true,
+  };
+  const susan: User = {
+    id: 1,
+    name: 'susan',
+    isActive: false,
+  };
+  
+  function createUser(user: User): User {
+    console.log(`Hello there ${user.name.toUpperCase()} !!!`);
+  
+    return user;
+  }
+
+  type StringOrNumber = string |number; 
+
+  let value: StringOrNumber;
+  value ='hello';
+  value = 123;
+
+  type Theme = 'light' | 'dark';
+
+  let theme: Theme;
+  theme = 'light';
+  theme = 'dark';
+
+  function setTheme(t:Theme){
+    theme = t;
+  }
+
+  setTheme('dark');
