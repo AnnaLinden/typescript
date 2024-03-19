e08da664cc6fae3dc0463c03329e90812f826a2e
AnnaLinden <linden.a.anna@gmail.com>
2024-03-19 10:45:18 +0200

    alias tutorial

--- src/tutorial.ts
+++ src/tutorial.ts
@@ -250,3 +250,24 @@ const john: User = {
   }
 
   setTheme('dark');
+
+  // challenge
+
+  type Employee = {id: number; name: string; department: string};
+  type Manager = {id: number; name: string; employees: Employee[]};
+  type Staff = Employee | Manager;
+
+  function printStaffDetails(staff: Staff){
+    if ('employees' in staff){
+        console.log(`${staff.name} is a Manager and has ${staff.employees.length} employees.`);
+    } else {
+        console.log(`${staff.name} is an employee of ${staff.department} department.`);
+    }
+  }
+
+  const alice: Employee = { id: 1, name: 'Alice', department: 'Sales' };
+  const steve: Employee = { id: 1, name: 'Steve', department: 'HR' };
+  const bob: Manager = { id: 2, name: 'Bob', employees: [alice, steve] };
+
+  printStaffDetails(alice); 
+  printStaffDetails(bob);
